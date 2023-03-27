1717bed89605bbe77c689454d6b201067b587429
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2023-03-27 11:42:08 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
+// import { App } from 'components/App';
 // import { App } from 'components/App1/App1';
+// import { App } from 'components/App2/App';
+// import { App } from 'components/App3/App';
+import { App } from 'components/01-components/App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-    {/* <App /> */}
   </React.StrictMode>
 );
